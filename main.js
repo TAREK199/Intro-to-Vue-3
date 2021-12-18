1e330abe96b6015bd15c7080059e12bf16bfbe34
rb.tarek <rebaitarek54@gmail.com>
2021-12-18 11:24:33 +0100

    commit to l7-start

--- main.js
+++ main.js
@@ -4,16 +4,14 @@ const app = Vue.createApp({
 
      data(){
         return{
-
+            brand:'Vue Mastery',
             product:'Socks',
-            inventory: 0,
-            inStock:false,
-            image:'./assets/images/socks_blue.jpg',
             cart:0,
+            selectedVariant:0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green',image:'./assets/images/socks_blue.jpg'},
-                { id: 2235, color: 'blue',image:'./assets/images/socks_green.jpg'}
+                { id: 2234, color: 'green',image:'./assets/images/socks_blue.jpg',quantity:50},
+                { id: 2235, color: 'blue',image:'./assets/images/socks_green.jpg',quantity:0}
               ]
         }
      },
@@ -22,9 +20,25 @@ const app = Vue.createApp({
         addToCart(){
             this.cart +=1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
         }
+     },
+
+     computed:{
+        title(){
+            return this.brand +' '+this.product
+        },
+
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        }
+
+
 
 
      }
